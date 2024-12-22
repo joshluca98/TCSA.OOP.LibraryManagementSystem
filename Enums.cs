c121121bf34054dab7369c76dd92e43a626df2b7
joshluca98 <jl_128_cs@pm.me>
2024-12-22 01:54:09 -0800

    ch13 complete

---
+++ Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace TCSA.OOP.LibraryManagementSystem;
+
+internal class Enums
+{
+    internal enum MenuOption
+    {
+        ViewBooks,
+        AddBook,
+        DeleteBook
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,31 +1,4 @@
-﻿using Spectre.Console;
-using TCSA.OOP.LibraryManagementSystem;
+﻿using TCSA.OOP.LibraryManagementSystem;
 
-var menuChoices = new string[3] { "View Books", "Add Book", "Delete Book" };
-
-while (true)
-{
-    Console.Clear();
-    var choice = AnsiConsole.Prompt(new SelectionPrompt<MenuOption>()
-        .Title("What do you want to do next?")
-        .AddChoices(Enum.GetValues<MenuOption>()));
-    switch (choice)
-    {
-        case MenuOption.ViewBooks:
-            BooksController.ViewBooks();
-            break;
-        case MenuOption.AddBook:
-            BooksController.AddBook();
-            break;
-        case MenuOption.DeleteBook:
-            BooksController.DeleteBook();
-            break;
-    }
-}
-
-enum MenuOption
-{
-    ViewBooks,
-    AddBook,
-    DeleteBook
-}
+UserInterface userInterface = new UserInterface();
+userInterface.MainMenu();
